97b8c15db8c2d2388a013ebc9edbc680f13a148e
I578412 <saptarshi.basu01@sap.com>
2025-09-05 14:57:13 +0530

    calculator made

--- src/main/java/com/example/DockerFirst/DockerFirstApplication.java
+++ src/main/java/com/example/DockerFirst/DockerFirstApplication.java
@@ -6,8 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DockerFirstApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DockerFirstApplication.class, args);
-	}
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int mul(int a,int b){
+        return a*b;
+    }
+    public static int div(int a,int b){
+        return a/b;
+    }
+    public static int min(int a,int b){
+        return a-b;
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(DockerFirstApplication.class, args);
+        System.out.println("App running... sum(2,3) = " + add(2, 3));
+    }
 }
--- src/test/java/com/example/DockerFirst/DockerFirstApplicationTests.java
+++ src/test/java/com/example/DockerFirst/DockerFirstApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.DockerFirst;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DockerFirstApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void testAdd() {
+        // testing the add method
+        int result = DockerFirstApplication.add(2, 3);
+        assertEquals(5, result, "2 + 3 should equal 5");
 
+        int result1 = DockerFirstApplication.add(10,5);
+        assertEquals(15, result, "10 + 5 should equal 15");
+
+        int result2 = DockerFirstApplication.mul(10,5);
+        assertEquals(50, result, "10 * 5 should equal 50");
+    }
 }
